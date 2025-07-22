1d4ad882636c12a20c969c07c3897201e3ebbefe
guang <guangzaishangwa@gmail.com>
2025-07-22 21:29:45 +0900

    Add base option to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: 'src',  // ここでrootをsrcに指定
+  root: 'src',
+  base: './', // ここを追加
   build: {
     outDir: '../dist',
     emptyOutDir: true,
